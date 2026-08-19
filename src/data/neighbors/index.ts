1d2fb410bd250ab93c92fb07a0da7ea3332fa401
Benjamin Haddon <haddonbenjamin@gmail.com>
2026-08-19 14:14:31 -0400

    add neighbor data access

---
+++ src/data/neighbors/index.ts
@@ -0,0 +1,8 @@
+import { getAfricaMapScopeConfig } from '../map-scopes.js';
+export {
+  AFRICA_LAND_ADJACENCY,
+  AFRICA_STANDARD_NEIGHBOR_TARGET_IDS,
+  AFRICA_ZERO_LAND_NEIGHBOR_IDS,
+} from './africa.js';
+
+export const getAfricaNeighborScopeConfig = getAfricaMapScopeConfig;
